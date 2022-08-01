0d025a18b87956c96565709b65809787db499396
Mrinal <dmrinal626@gmail.com>
2022-08-01 23:24:08 +0530

    Time: 35 ms (85.71%), Space: 13.9 MB (88.52%) - LeetHub

---
+++ 1201-ugly-number-iii/1201-ugly-number-iii.py
@@ -0,0 +1,24 @@
+class Solution:
+    def nthUglyNumber(self, n: int, a: int, b: int, c: int) -> int:
+        left = min(a,b,c)
+        right = 10**10
+        
+        ab = (a*b) // math.gcd(a,b) #find lcm ab
+        bc = (b*c) // math.gcd(b,c)
+        ac = (a*c) // math.gcd(a,c)
+        abc = (a*bc) // math.gcd(a,bc)
+        
+        def condition(ugly):
+            num = ugly//a + ugly // b + ugly // c - ugly //ab - ugly//ac - ugly // bc +  ugly/abc
+            return num >= n
+        
+        while left < right:
+            mid = left + (right - left) // 2
+            
+            if condition(mid):
+                right = mid
+            else:
+                left = mid + 1
+        
+        return left
+        
