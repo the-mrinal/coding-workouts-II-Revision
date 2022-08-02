c8163b75398f83289a420d8ecf3590d6dfcd8571
Mrinal <dmrinal626@gmail.com>
2022-08-02 22:43:29 +0530

    Time: 399 ms (51.19%), Space: 24.8 MB (24.67%) - LeetHub

--- 1387-sort-integers-by-the-power-value/1387-sort-integers-by-the-power-value.py
+++ 1387-sort-integers-by-the-power-value/1387-sort-integers-by-the-power-value.py
@@ -1,14 +1,18 @@
 class Solution:
     def getKth(self, lo: int, hi: int, k: int) -> int:
         arr = []
-        
+        cache = defaultdict(int)
         def findPower(num):
+            if num in cache:
+                return cache[num]
             if num == 1:
                 return 0
             if num % 2 == 0:
-                return 1 + findPower(num/2)
+                cache[num] = 1 + findPower(num/2)
             else:
-                return 1 + findPower(3*num + 1)
+                cache[num] = 1 + findPower(3*num + 1)
+            
+            return cache[num]
         
         for num in range(lo,hi + 1):
             power = findPower(num)
