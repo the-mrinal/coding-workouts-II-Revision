8f4e0010d9eaebc159084c6f83905bba0948ab63
Mrinal <dmrinal626@gmail.com>
2022-08-04 00:21:56 +0530

    Time: 8614 ms (22.26%), Space: 14.1 MB (43.69%) - LeetHub

--- 16-3sum-closest/16-3sum-closest.py
+++ 16-3sum-closest/16-3sum-closest.py
@@ -1,21 +1,47 @@
 class Solution:
     def threeSumClosest(self, nums: List[int], target: int) -> int:
         nums.sort()
-        result = float('inf')
-        for  i in range(len(nums) - 2):
-            j , k = i + 1,len(nums) - 1
+        n = len(nums)
+        res = float('inf')
+        for i in range(n - 2):
+            left = i + 1
+            right = n - 1
             
-            while j < k:
+            while left < right:
+                currSum = nums[i] + nums[left] + nums[right]
                 
-                currSum = nums[i] + nums[j] + nums[k]
                 if currSum == target:
-                    return currSum
-                if abs(currSum - target) < abs(result - target):
-                    result = currSum
+                    return target
+                
+                if abs(currSum - target) < abs(res - target):
+                    res = currSum
                 
                 if currSum < target:
-                    j += 1
+                    left += 1
                 elif currSum > target:
-                    k -= 1
+                    right -= 1
+            
+        return res
+    
+
+#     class Solution:
+#     def threeSumClosest(self, nums: List[int], target: int) -> int:
+#         nums.sort()
+#         result = float('inf')
+#         for  i in range(len(nums) - 2):
+#             j , k = i + 1,len(nums) - 1
+            
+#             while j < k:
+                
+#                 currSum = nums[i] + nums[j] + nums[k]
+#                 if currSum == target:
+#                     return currSum
+#                 if abs(currSum - target) < abs(result - target):
+#                     result = currSum
+                
+#                 if currSum < target:
+#                     j += 1
+#                 elif currSum > target:
+#                     k -= 1
         
-        return result
+#         return result
