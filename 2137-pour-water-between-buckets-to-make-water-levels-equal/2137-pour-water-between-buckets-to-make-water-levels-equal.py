08c9b8ac4676d1abd930efd81a927f73596e6f9e
Mrinal <dmrinal626@gmail.com>
2022-08-05 19:03:27 +0530

    Time: 2879 ms (27.87%), Space: 25.9 MB (31.15%) - LeetHub

---
+++ 2137-pour-water-between-buckets-to-make-water-levels-equal/2137-pour-water-between-buckets-to-make-water-levels-equal.py
@@ -0,0 +1,30 @@
+class Solution:
+    def equalizeWater(self, buckets: List[int], loss: int) -> float:
+        
+        def condition(x):
+            out = 0
+            ind = 0
+            for w in buckets:
+                if w > x:
+                    out += (w - x)
+                else:
+                    ind += (x - w)
+            if (out - (out * (loss/100))) >= ind:
+                return False
+            return True
+        
+        left = 0
+        right = max(buckets)
+        
+        ellipsion = 10**-5
+        
+        while right - left > ellipsion: # till the diff beetween them is greater than 0.00005
+            mid = float(left + (right - left) / 2)
+            
+            if condition(mid):
+                right = mid
+            else:
+                left = mid
+        
+        return left
+        
