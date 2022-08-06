d337c363f96d34bf945736c65028fc840f176fd7
Mrinal <dmrinal626@gmail.com>
2022-08-07 02:31:09 +0530

    Time: 509 ms (53.01%), Space: 39.6 MB (26.13%) - LeetHub

---
+++ 698-partition-to-k-equal-sum-subsets/698-partition-to-k-equal-sum-subsets.py
@@ -0,0 +1,42 @@
+class Solution:
+    def canPartitionKSubsets(self, nums: List[int], k: int) -> bool:
+        total = sum(nums)
+        
+        n = len(nums)
+        
+        if total % k != 0:
+            return False
+        
+        target = total // k
+        
+        taken = ['0'] * n
+        
+        nums.sort(reverse=True)
+        cache = {}
+        def backtrack(index,count,currSum):
+            nonlocal n
+            if count == k - 1:
+                return True # total is divisible by k
+            
+            if currSum > target:
+                return False
+            
+            key = ''.join(taken)
+            
+            if key not in cache:
+                if currSum == target:
+                    cache[key] = backtrack(0,count + 1,0)
+                    return cache[key]
+
+                for i in range(index,n):
+                    if not taken[i] == '1':
+                        taken[i] = '1'
+
+                        if backtrack(i + 1,count,currSum + nums[i]):
+                            return True
+
+                        taken[i] = '0'
+            cache[key] = False
+            return cache[key]
+        
+        return backtrack(0,0,0)
