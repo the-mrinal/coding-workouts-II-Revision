133dece971d2ba71f42f4803a1923428e82570b1
Mrinal <dmrinal626@gmail.com>
2022-08-13 23:52:11 +0530

    Time: 44 ms (60.86%), Space: 13.8 MB (95.87%) - LeetHub

---
+++ 357-count-numbers-with-unique-digits/357-count-numbers-with-unique-digits.py
@@ -0,0 +1,10 @@
+class Solution:
+    def countNumbersWithUniqueDigits(self, n: int) -> int:
+        if n == 0:
+            return 1
+        total ,start = 10,9
+        
+        for i in range(1,n):
+            start = start *(10 - i)
+            total += start
+        return total
