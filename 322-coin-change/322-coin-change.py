87f2a4ba332e56001f3da6ec2d334caf2860a966
Mrinal <dmrinal626@gmail.com>
2022-08-13 20:28:01 +0530

    Time: 1716 ms (78.61%), Space: 14.3 MB (42.47%) - LeetHub

--- 322-coin-change/322-coin-change.py
+++ 322-coin-change/322-coin-change.py
@@ -1,25 +1,35 @@
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
         n = len(coins)
-        cache = {}
-        def findCount(target):
-            if target == 0:
-                cache[target] = 0
-                return 0
-            if target < 0:
-                return float('inf')
-            
-            key = target
-            
-            if key not in cache:
-                ans = float('inf')
-                for coin in coins:
-                    ans = min(ans,1 + findCount(target - coin))
-                cache[key] = ans
-            
-            return cache[key]
+        dp = [float('inf') for _ in range(amount + 1)]
+        dp[0] = 0
         
-        findCount(amount)
+        for coin in coins:
+            for j in range(coin,amount + 1):
+                dp[j] = min(dp[j],dp[j - coin] + 1)
+        return dp[amount] if dp[amount] < float('inf') else -1
+                
+            
         
-        return cache[amount] if cache[amount] < float('inf') else -1
+#         cache = {}
+#         def findCount(target):
+#             if target == 0:
+#                 cache[target] = 0
+#                 return 0
+#             if target < 0:
+#                 return float('inf')
+            
+#             key = target
+            
+#             if key not in cache:
+#                 ans = float('inf')
+#                 for coin in coins:
+#                     ans = min(ans,1 + findCount(target - coin))
+#                 cache[key] = ans
+            
+#             return cache[key]
+        
+#         findCount(amount)
+        
+#         return cache[amount] if cache[amount] < float('inf') else -1
     
