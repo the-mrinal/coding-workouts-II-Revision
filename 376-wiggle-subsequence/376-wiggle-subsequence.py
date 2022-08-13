8a772a92af1cf53bbddfbdf28af26702285963ab
Mrinal <dmrinal626@gmail.com>
2022-08-14 01:10:22 +0530

    Time: 228 ms (10.43%), Space: 13.8 MB (98.47%) - LeetHub

--- 376-wiggle-subsequence/376-wiggle-subsequence.py
+++ 376-wiggle-subsequence/376-wiggle-subsequence.py
@@ -1,29 +1,14 @@
 class Solution:
     def wiggleMaxLength(self, nums: List[int]) -> int:
         n = len(nums)
+        up = [0] * n
+        down = [0] * n
         
-        cache = {}
+        for i in range(n):
+            for j in range(0,i):
+                if nums[i] < nums[j]:
+                    down[i] = max(down[i],up[j] + 1)
+                elif nums[i] > nums[j]:
+                    up[i] = max(up[i],down[j] + 1)
         
-        def findMaxWiggle(curr,prev,wasPos):
-            if curr == n:
-                return 1
-            
-            key = (curr,prev,wasPos)
-            
-            if key not in cache:
-                ans1 , ans2 = 0,0
-                if prev != curr and wasPos and nums[curr] < nums[prev]:
-                    ans1 = 1 + findMaxWiggle(curr + 1,curr,False)
-                elif prev != curr and not wasPos and nums[curr] > nums[prev]:
-                    ans1 = 1 + findMaxWiggle(curr + 1,curr,True)
-
-                ans2 = findMaxWiggle(curr + 1,prev,wasPos)
-                
-                cache[key] = max(ans1,ans2)
-            
-            return cache[key]
-        
-        findMaxWiggle(0,0,False)
-        findMaxWiggle(0,0,True)        
-        # print(cache)
-        return max(cache[(0,0,True)],cache[(0,0,False)])
+        return 1 + max(up[n - 1],down[n - 1])
