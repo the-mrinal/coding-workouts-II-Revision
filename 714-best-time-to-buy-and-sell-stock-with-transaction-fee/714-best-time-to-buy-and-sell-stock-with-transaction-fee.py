3e3142b49ac14c3d4f90a18e324e7f43977f476e
Mrinal <dmrinal626@gmail.com>
2022-08-15 19:00:33 +0530

    Time: 1147 ms (60.78%), Space: 21.2 MB (85.37%) - LeetHub

--- 714-best-time-to-buy-and-sell-stock-with-transaction-fee/714-best-time-to-buy-and-sell-stock-with-transaction-fee.py
+++ 714-best-time-to-buy-and-sell-stock-with-transaction-fee/714-best-time-to-buy-and-sell-stock-with-transaction-fee.py
@@ -1,23 +1,10 @@
 class Solution:
     def maxProfit(self, prices: List[int], fee: int) -> int:
         n = len(prices)
-        cache = {}
-        def dfs(index,bought):
-            if index == n:
-                return 0
-            key = (index,bought)
-            if key not in cache:
-                if bought:
-                    ans1 = prices[index] - fee + dfs(index + 1,False)
-                elif bought == False:
-                    ans1 = -prices[index] + dfs(index + 1,True)
-
-                ans2 = dfs(index + 1,bought)
-                cache[key] = max(ans1,ans2)
-                
-                
-            return cache[key]
+        cash,hold = 0,-prices[0]
         
-        dfs(0,False)
-        return cache[(0,False)]
-            
+        for i in range(1,n):
+            cash = max(cash,hold + prices[i] - fee)
+            hold = max(hold,cash - prices[i])
+        
+        return cash
