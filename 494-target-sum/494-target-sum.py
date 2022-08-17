30f0e41e2b282f485b9460824f0510a50aa82630
Mrinal <dmrinal626@gmail.com>
2022-08-17 23:46:47 +0530

    Time: 612 ms (32.00%), Space: 36.1 MB (14.75%) - LeetHub

--- 494-target-sum/494-target-sum.py
+++ 494-target-sum/494-target-sum.py
@@ -1,27 +1,22 @@
 class Solution:
-    
-    def helper(self,nums,target,index,curr_sum,cache):
-        if (index,curr_sum) in cache:
-            return cache[(index,curr_sum)]
-        
-        if index < 0 and target == curr_sum:
-            return 1
-        elif index < 0:
-            return 0
-        
-        
-        
-        countN = self.helper(nums,target,index - 1,curr_sum - nums[index],cache)
-        
-        countP = self.helper(nums,target,index - 1,curr_sum + nums[index],cache)
-        
-        cache[(index,curr_sum)] = countN + countP
-        
-        return cache[(index,curr_sum)]
-    
-    
     def findTargetSumWays(self, nums: List[int], target: int) -> int:
+        n = len(nums)
         cache = {}
-        curr_sum = 0
-        index = len(nums) - 1
-        return self.helper(nums,target,index,curr_sum,cache)
+        
+        def dfs(index,currSum):
+            if currSum == 0 and index == n:
+                return 1
+            
+            if index >= n:
+                return 0
+            
+            key = (index,currSum)
+            
+            if key not in cache:
+                sum1 = dfs(index + 1,currSum + nums[index])
+                sum2 = dfs(index + 1,currSum - nums[index])
+                cache[key] = sum1 + sum2
+            
+            return cache[key]
+        
+        return dfs(0,target)
