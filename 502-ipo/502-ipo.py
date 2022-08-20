b4ab654ab79e8ba4cc2f77b027bd484b4740c50f
Mrinal <dmrinal626@gmail.com>
2022-08-21 01:14:31 +0530

    Time: 2250 ms (18.47%), Space: 37.4 MB (80.88%) - LeetHub

---
+++ 502-ipo/502-ipo.py
@@ -0,0 +1,26 @@
+class Solution:
+    def __init__(self):
+        self.minCapHeap = []
+        self.maxAvailProfit = []
+    
+    
+    def findMaximizedCapital(self, k: int, w: int, profits: List[int], capital: List[int]) -> int:
+        maxProfit = 0
+        
+        for index in range(len(capital)):
+            heappush(self.minCapHeap,[capital[index],index])
+        
+        availCapital = w
+        
+        
+        
+        for i in range(k):
+            while self.minCapHeap and self.minCapHeap[0][0] <= availCapital:
+                cap,index = heappop(self.minCapHeap)
+                heappush(self.maxAvailProfit,[-profits[index],cap])
+            
+            if not self.maxAvailProfit:
+                break
+            availCapital += -heappop(self.maxAvailProfit)[0]
+        
+        return availCapital
