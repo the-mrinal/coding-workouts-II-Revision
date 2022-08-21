dcc37bf98af30bbe89c2aa0812671b7fee8d2dd9
Mrinal <dmrinal626@gmail.com>
2022-08-21 14:29:31 +0530

    Time: 1156 ms (43.54%), Space: 50.4 MB (69.73%) - LeetHub

--- 1202-smallest-string-with-swaps/1202-smallest-string-with-swaps.py
+++ 1202-smallest-string-with-swaps/1202-smallest-string-with-swaps.py
@@ -1,32 +1,34 @@
 class UnionFind:
     def __init__(self,size):
         self.root = [i for i in range(size)]
-        self.rank = [1 for i in range(size)]
+        self.rank = [1 for _ in range(size)]
+        self.count = size - 1
     
     def find(self,x):
-        if self.root[x] == x:
+        if x == self.root[x]:
             return x
         self.root[x] = self.find(self.root[x])
         return self.root[x]
     
-    def insert(self,a,b):
+    
+    def union(self,a,b):
         rootA = self.find(a)
         rootB = self.find(b)
         
         if rootA != rootB:
             if self.rank[rootA] > self.rank[rootB]:
                 self.root[rootB] = rootA
-            elif self.rank[rootA] < self.rank[rootB]:
+            elif self.rank[rootB] > self.rank[rootA]:
                 self.root[rootA] = rootB
             else:
-                self.root[rootA] = rootB
+                self.root[rootB] = rootA
                 self.rank[rootA] += 1
     
     def isConnected(self,a,b):
         return self.find(a) == self.find(b)
-        
-        
-        
+    
+
+
 class Solution:
     def smallestStringWithSwaps(self, s: str, pairs: List[List[int]]) -> str:
         n = len(s)
@@ -34,54 +36,22 @@ class Solution:
         uf = UnionFind(n)
         
         for a,b in pairs:
-            uf.insert(a,b)
+            uf.union(a,b)
         
+        hashMap = defaultdict(list)
         
-        pairMap = defaultdict(list)
-        
-        for i in range(len(s)):
-            parent = uf.find(i)
-            
-            pairMap[parent].append([i,s[i]])
-        
-        new_s = [0]*len(s)
-        
-        for key in pairMap:
-            curr = pairMap[key]
-            values = curr.copy()
-            curr.sort(key = lambda x: x[1])
-            
-            for i in range(len(values)):
-                new_s[values[i][0]] = curr[i][1]
-        
+        for i in range(n):
+            rootI = uf.find(i)
+            hashMap[rootI].append([s[i],i])
+        new_s = [0 for _ in range(n)]
+        for R,values in hashMap.items():
+            indexToUse = values.copy()
+            sortedLetters = sorted(values)
+            i = 0
+            for _,index in indexToUse:
+                new_s[index] = sortedLetters[i][0]
+                i += 1
         return ''.join(new_s)
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-        
-        
-        
-        
         
         
         
