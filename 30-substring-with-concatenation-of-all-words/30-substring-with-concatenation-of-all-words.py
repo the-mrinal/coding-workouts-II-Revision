6bce78ee5f224bd3fe7f7ce5650734966d5d4992
Mrinal <dmrinal626@gmail.com>
2022-08-23 00:04:55 +0530

    Time: 1096 ms (23.01%), Space: 14.1 MB (75.98%) - LeetHub

--- 30-substring-with-concatenation-of-all-words/30-substring-with-concatenation-of-all-words.py
+++ 30-substring-with-concatenation-of-all-words/30-substring-with-concatenation-of-all-words.py
@@ -1,35 +1,25 @@
 class Solution:
     def findSubstring(self, s: str, words: List[str]) -> List[int]:
-
-        n = len(s)
-        word_len = len(words[0])
-        n_word = len(words)
-
-        total_len = n_word * word_len
-
-
-        words.sort()	
-
+        words.sort()
+        k = len(words)
+        k_word = len(words[0])
+        total_len = k_word * k
         def validate(start,end):
-            word_list = s[start:end + 1]
-            word_list = [word_list[i:i+word_len] for i in range(0,total_len,word_len)]
-            word_list.sort()
-
-            for index in range(n_word):
-                if word_list[index] != words[index]:
+            curr_words = s[start:end+1]
+            curr_word_list = [curr_words[i:i + k_word] for i in range(0,total_len,k_word)]
+            curr_word_list.sort()
+            # print(curr_word_list,curr_words)
+            for i in range(k):
+                if words[i] != curr_word_list[i]:
                     return False
-
+            
             return True
-
-
-
-
+        result = []
         start = 0
-        ans = []
-        for end in range(n):
-            if end - start + 1 == total_len:
-                if validate(start,end):
-                    ans.append(start)
+        for i in range(len(s)):
+            if i - start + 1 == total_len:
+                if validate(start,i):
+                    result.append(start)
                 start += 1
-
-        return ans
+        
+        return result
