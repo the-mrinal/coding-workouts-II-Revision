b8db7217a0c405373f365b61f9a8f83bab777fd5
Mrinal <dmrinal626@gmail.com>
2022-08-28 01:50:23 +0530

    Time: 55 ms (27.90%), Space: 13.9 MB (21.86%) - LeetHub

--- 276-paint-fence/276-paint-fence.py
+++ 276-paint-fence/276-paint-fence.py
@@ -1,12 +1,14 @@
 class Solution:
     def numWays(self, n: int, k: int) -> int:
         if n <= 2:
-            return k ** n
-        dp = [0 for _ in range(n + 1)]
-        dp[1] = k ** 1
-        dp[2] = (k ** 2) 
-        for i in range(3,n + 1):
-            dp[i] = (k-1)*(dp[i - 1] + dp[i -2])
+            return k**(n)
+        dp = [0 for _ in range(n+1)]
+        dp[0] = k
+        dp[1] = k*k
         
-        return dp[n]
+        for i in range(2,n):
+            dp[i] = (k-1)*(dp[i-1] + dp[i - 2])
+            
         
+        
+        return dp[n - 1]
